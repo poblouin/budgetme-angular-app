f5f1872c0776730e564ae583cb206145f6e00fed
poblouin <blouin.pierreolivier@gmail.com>
2018-02-26 18:14:31 -0500

    Fixed bug with token when creating new account

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -13,90 +13,93 @@ import { User } from '../models';
 
 @Injectable()
 export class UserService {
-  private currentUserSubject = new BehaviorSubject<User>(new User());
-  private isAuthenticatedSubject = new ReplaySubject<boolean>(1);
+    private currentUserSubject = new BehaviorSubject<User>(new User());
+    private isAuthenticatedSubject = new ReplaySubject<boolean>(1);
 
-  public currentUser = this.currentUserSubject.asObservable().distinctUntilChanged();
-  public isAuthenticated = this.isAuthenticatedSubject.asObservable();
+    public currentUser = this.currentUserSubject.asObservable().distinctUntilChanged();
+    public isAuthenticated = this.isAuthenticatedSubject.asObservable();
 
-  constructor(
-    private apiService: ApiService,
-    private http: Http,
-    private jwtService: JwtService
-  ) { }
+    constructor(
+        private apiService: ApiService,
+        private http: Http,
+        private jwtService: JwtService
+    ) { }
 
-  getCurrentUser(): User {
-    return this.currentUserSubject.value;
-  }
-
-  removeUser() {
-    this.jwtService.destroyToken();
-    this.currentUserSubject.next(new User());
-    this.isAuthenticatedSubject.next(false);
-  }
-
-  populate() {
-    if (this.jwtService.getToken()) {
-      this.apiService.get('/user')
-        .subscribe(
-        data => this.setUser(data.user),
-        err => this.removeUser()
-        );
-    } else {
-      this.removeUser();
+    getCurrentUser(): User {
+        return this.currentUserSubject.value;
     }
-  }
 
-  login(credentials: Object): Observable<User> {
-    return this.obtainToken(credentials)
-      .flatMap(
-        res => {
-          return this.apiService.get('/user')
-            .map(
-            data => {
-              this.setUser(data.user);
-              return data;
-            },
-            err => {
-              this.removeUser();
-            });
+    removeUser() {
+        this.jwtService.destroyToken();
+        this.currentUserSubject.next(new User());
+        this.isAuthenticatedSubject.next(false);
+    }
+
+    populate() {
+        if (this.jwtService.getToken()) {
+            this.apiService.get('/user').subscribe(
+                data => this.setUser(data.user),
+                err => this.removeUser()
+            );
+        } else {
+            this.removeUser();
         }
+    }
+
+    login(credentials: Object): Observable<User> {
+        return this.obtainToken(credentials).flatMap(
+            res => {
+                return this.apiService.get('/user').map(
+                    data => {
+                        this.setUser(data.user);
+                        return data;
+                    },
+                    err => {
+                        this.removeUser();
+                    });
+            }
         );
-  }
+    }
 
-  register(credentials: Object): Observable<User> {
-    return this.apiService.post('/users', { user: credentials })
-      .map(
-        data => {
-          this.setUser(data.user);
-          this.obtainToken(credentials)
-          return data;
-        }
-      );
-  }
+    register(credentials: Object): Observable<User> {
+        return this.apiService.post('/users', { user: credentials }).flatMap(
+            data => {
+                this.setUser(data.user);
+                return this.obtainToken(credentials).map(
+                    hasToken => {
+                        if (!hasToken) {
+                            this.removeUser();
+                            return {};
+                        } else {
+                            return data;
+                        }
+                    }
+                );
+            }
+        );
+    }
 
-  update(user): Observable<User> {
-    return this.apiService.put('/user', { user })
-      .map(data => {
-        this.currentUserSubject.next(data.user);
-        return data.user;
-      }
-      );
-  }
+    update(user): Observable<User> {
+        return this.apiService.put('/user', { user }).map(
+            data => {
+                this.currentUserSubject.next(data.user);
+                return data.user;
+            }
+        );
+    }
 
-  private setUser(user: User) {
-    this.currentUserSubject.next(user);
-    this.isAuthenticatedSubject.next(true);
-  }
+    private setUser(user: User) {
+        this.currentUserSubject.next(user);
+        this.isAuthenticatedSubject.next(true);
+    }
 
-  private obtainToken(credentials: Object): Observable<Boolean> {
-    return this.apiService.post('/obtain-token', credentials)
-      .map(
-        data => {
-          this.jwtService.saveToken(data.token);
-          return true;
-        }
-      );
-  }
+    private obtainToken(credentials: Object): Observable<Boolean> {
+        return this.apiService.post('/obtain-token', credentials).map(
+            data => {
+                this.jwtService.saveToken(data.token);
+                return true;
+            }
+        );
+    }
 
 }
